710117a458d7bd5c52bf0bee16348ed0aa9fb386
StephenMorara <stevegrapher@gmail.com>
2025-08-17 22:00:37 +0300

    feat: Script to list tables in alx_book_store (Task 3)

---
+++ task_3.sql
@@ -0,0 +1,6 @@
+-- task_3.sql
+-- This script lists all tables in the alx_book_store database.
+-- The database name is expected to be passed as an argument to the mysql command.
+
+-- Command to show all tables in the currently selected database.
+SHOW TABLES;
