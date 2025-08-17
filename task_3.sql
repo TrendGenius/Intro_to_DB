86ae508f93d35ccf1b4778b4ea125e2d6274a582
StephenMorara <stevegrapher@gmail.com>
2025-08-17 22:03:45 +0300

    Fix: Add USE statement to task_3.sql for checker

--- task_3.sql
+++ task_3.sql
@@ -2,5 +2,8 @@
 -- This script lists all tables in the alx_book_store database.
 -- The database name is expected to be passed as an argument to the mysql command.
 
+-- Select the alx_book_store database to ensure subsequent commands operate on it.
+USE alx_book_store;
+
 -- Command to show all tables in the currently selected database.
 SHOW TABLES;
